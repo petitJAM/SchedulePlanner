9d3631fbac971bff5c6f0db55fbce1f413512ae0
Alex Petitjean <petitjam@rose-hulman.edu>
2013-01-23 23:28:40 -0500

    dumb thing

--- server/main.py
+++ server/main.py
@@ -20,5 +20,4 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.out.write('Hello world!')
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
-                              debug=True)
+app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
