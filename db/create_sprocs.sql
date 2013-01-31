0ad9850f7a544b5a7dac4dcd7b2195082a8ac40e
Alex Petitjean <petitjam@rose-hulman.edu>
2013-01-30 21:03:47 -0500

    adduser, addschedule, additem sprocs

--- db/create_sprocs.sql
+++ db/create_sprocs.sql
@@ -13,15 +13,43 @@ CREATE PROCEDURE countusers (OUT param1 INT)
 BEGIN
 	SELECT COUNT(*) INTO param1 FROM `users`;
 END$$
-
+ 
 
 # Add user
 DROP PROCEDURE IF EXISTS adduser$$
-CREATE PROCEDURE adduser (@name varchar(45), email varchar(255), password varchar(255))
+CREATE PROCEDURE adduser (name varchar(45), email varchar(255), password varchar(255))
 BEGIN
 	insert into `users` (`Name`, `Email`, `Password`, `Active_SID`)
-				 values (@name, email, sha1(password), Null);
+				 values (name, email, sha1(password), Null);
 END$$
 
 
+# Add schedule
+DROP PROCEDURE IF EXISTS addschedule$$
+CREATE PROCEDURE addschedule (userID INT, sdate DATE, edate DATE)
+BEGIN
+	insert into `schedules` (`UID`, `Start_date`, `End_date`)
+					 values (userID, sdate, edate);
+END$$
+
+
+# Add Item
+DROP PROCEDURE IF EXISTS additem$$
+CREATE PROCEDURE additem (schedID INT, courseID INT, completeby DATETIME, priority INT, notes TEXT, diff INT)
+BEGIN
+	insert into `items` (`SID`, `CID`, `Complete_by`, `Priority`, `Notes`, `Difficulty`)
+				 values (schedID, courseID, completeby, priority, notes, diff);
+END$$
+
+
+
+
 delimiter ;
+
+/*
+DROP PROCEDURE IF EXISTS sproc_name$$
+CREATE PROCEDURE sproc_name ()
+BEGIN
+	#queries
+END$$
+*/
