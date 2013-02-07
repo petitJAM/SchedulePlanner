e61e835f009b6b675bc65ab3d7d771ba93b9e56a
Vismay <itsvismay@gmail.com>
2013-02-06 19:39:17 -0500

    working on templating

--- server/main.py
+++ server/main.py
@@ -18,8 +18,22 @@
 import webapp2
 
 import cgi
+import os
 import re
 import MySQLdb
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+
 
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
